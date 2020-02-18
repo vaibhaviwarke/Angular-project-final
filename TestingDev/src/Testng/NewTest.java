f80a6610a53b4ca546f8bbc365b369305b35f9af
vaibhaviwarke <Vaibhavi.Warke@facileserv.local>
2020-02-18 16:35:26 +0530

    Update

--- TestingDev/src/Testng/NewTest.java
+++ TestingDev/src/Testng/NewTest.java
@@ -11,8 +11,8 @@ public class NewTest {
 	WebDriver driver;
 	@BeforeTest
 	  public void beforeTest() throws Exception {
-		  System.setProperty("webdriver.chrome.driver", "d:\\chromedriver.exe");
-			 driver=new ChromeDriver();
+		  System.setProperty("webdriver.chrome.driver", "C:\\Users\\vaibhavi.warke\\eclipse-workspace\\Angular-project-final\\TestingDev\\lib\\chromedriver.exe");
+			driver=new ChromeDriver();
 			driver.get("http://172.16.8.93/");
 			driver.findElement(By.xpath("//a[@class='btn btn-link']")).click();
 			Thread.sleep(1000);
@@ -20,7 +20,7 @@ public class NewTest {
 
   @Test
   public void f() throws Exception {
-	  driver.findElement(By.xpath("/html[1]/body[1]/app[1]/div[1]/div[1]/div[1]/div[1]/ng-component[1]/form[1]/div[1]/input[1]")).sendKeys("Omkar");
+	    driver.findElement(By.xpath("/html[1]/body[1]/app[1]/div[1]/div[1]/div[1]/div[1]/ng-component[1]/form[1]/div[1]/input[1]")).sendKeys("Omkar");
 		driver.findElement(By.xpath("//div[2]//input[1]")).sendKeys("More");
 		driver.findElement(By.xpath("//div[3]//input[1]")).sendKeys("Omii");
 		driver.findElement(By.xpath("//div[4]//input[1]")).sendKeys("Omkar123");
